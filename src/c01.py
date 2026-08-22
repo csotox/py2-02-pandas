c39a9726dbe0ef8403e9e3160413687de9f7ae76
Carlos Soto <odoo.soporte@gmail.com>
2026-08-22 14:36:26 -0400

    wip: fundamentos de pandas

---
+++ src/c01.py
@@ -0,0 +1,56 @@
+import pandas as pd
+
+def inicio():
+    print("Ejemplo de Serie de Pandas")
+
+    lista_precios = [100, 500, 600, 150, 400]
+
+    # print( type(lista_precios) )
+    # print( lista_precios )
+
+    precios = pd.Series(lista_precios)
+    print( type(precios) )
+    print( precios )
+
+    print()
+    print("Ejemplo de DataFrame de Pandas" )
+    venta = {
+        "producto": ["Producto 1", "Producto 2", "Producto 3"],
+        "precio": [10, 25, 18]
+    }
+    print( type(venta) )
+    print( venta )
+
+    df_venta = pd.DataFrame(venta)
+    print( type(df_venta) )
+    print( df_venta )
+
+def get_productos():
+    df = pd.read_csv("data/products.csv")
+
+    return df
+
+def main():
+    # print("Hola mundo")
+    # inicio()
+
+    df_productos = get_productos()
+    # print( df_productos )
+
+    # Imprime/muestra las primeras 5 filas
+    # print( df_productos.head() )
+    # print( df_productos.head(10) )
+
+    # Imprimir/mostrar las últimnas filas
+    # print( df_productos.tail() )
+    # print( df_productos.tail(15) )
+
+    # Obtener filas aleatorias
+    print( df_productos.sample(5) )
+
+
+
+
+
+if __name__ == "__main__":
+    main()
